6a036d2b7c43a8d2b4f631bbc6fdd6bd9babf5ed
mdxshakil <mdxshakil1767@gmail.com>
2023-07-13 21:47:03 +0600

    Added header component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,59 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/images/header-logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  const linkStyles =
+    "text-green-500 hover:text-green-700 transition-all ease-in-out";
+  return (
+    <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
+      <div className="h-full w-full bg-white/60">
+        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+          <div>
+            <Link to={"/"}>
+              <img className="h-20" src={logo} alt="log" />
+            </Link>
+          </div>
+          <div>
+            <ul className="flex items-center gap-6">
+              <li>
+                <Link to="/">
+                  <a className={linkStyles}>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/all-books">
+                  <a className={linkStyles}>All Books</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/add-new-book">
+                  <a className={linkStyles}>Add New Book</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/wish-list">
+                  <a className={linkStyles}>Wish List</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/reading-list">
+                  <a className={linkStyles}>Reading List</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  <a className={linkStyles}>Login</a>
+                </Link>
+              </li>
+              <li className="ml-5">
+                <a className={linkStyles}>profile</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
