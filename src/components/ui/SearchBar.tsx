f58d23b6aee3e1a90cef5c717c44c0520c55ae7d
mdxshakil <mdxshakil1767@gmail.com>
2023-07-15 10:01:54 +0600

    Added search functionality on the frontend

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,13 +1,33 @@
+import { useForm } from "react-hook-form";
 import { FaSearch } from "react-icons/fa";
 
+interface SearchFormInputs {
+  searchText: string;
+}
+
 const SearchBar = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+  } = useForm<SearchFormInputs>();
+
+  const onSubmit = (data: SearchFormInputs) => {
+    if (data.searchText === "") return;
+    
+    
+  };
+
   return (
     <div className="relative">
-      <input
-        type="text"
-        placeholder="Search"
-        className="border-2 rounded-full px-8 py-2 outline-none sm:w-64 md:w-80 lg:w-96"
-      />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Search"
+          className="border-2 rounded-full px-8 py-2 outline-none sm:w-64 md:w-80 lg:w-96"
+          {...register("searchText")}
+        />
+      </form>
       <div className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400">
         <FaSearch />
       </div>
--- src/page/AllBooks.tsx
+++ src/page/AllBooks.tsx
@@ -1,9 +1,10 @@
 import BookCard from "../components/BookCard";
 import Spinner from "../components/Spinner";
 import ErrorElement from "../components/ui/ErrorElement";
-import SearchBar from "../components/ui/SearchBar";
 import { useGetAllBooksQuery } from "../redux/features/book/book.api";
 import { IBook } from "../types/globalTypes";
+import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
 
 const AllBooks = () => {
   const {
@@ -12,18 +13,36 @@ const AllBooks = () => {
     isError,
     error,
   } = useGetAllBooksQuery(undefined);
+  const [searchText, setSearchText] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  let searchedBooks = [...(books?.data ?? [])];
+  if (searchText) {
+    searchedBooks = searchedBooks.filter((book: IBook) => {
+      const { genre, author, title } = book;
+      const searchTextLower = searchText.toLowerCase();
+      return (
+        genre.toLowerCase().includes(searchTextLower) ||
+        author.toLowerCase().includes(searchTextLower) ||
+        title.toLowerCase().includes(searchTextLower)
+      );
+    });
+  }
 
   let content = null;
   if (isLoading) {
     content = <Spinner />;
   } else if (!isLoading && isError && error) {
     content = <ErrorElement message="Failed to laod books" />;
-  } else if (!isLoading && !isError && !books?.data?.length) {
+  } else if (!isLoading && !isError && !searchedBooks.length) {
     content = <p>No books available</p>;
   } else {
     content = (
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-12 p-6">
-        {books?.data?.map((book: IBook) => (
+        {searchedBooks?.map((book: IBook) => (
           <BookCard book={book} key={book._id} />
         ))}
       </div>
@@ -33,7 +52,17 @@ const AllBooks = () => {
   return (
     <div>
       <div className="flex flex-col items-center gap-3 p-3">
-        <SearchBar />
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className="border-2 rounded-full px-8 py-2 outline-none sm:w-64 md:w-80 lg:w-96"
+            onChange={handleInputChange}
+          />
+          <div className="absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400">
+            <FaSearch />
+          </div>
+        </div>
         {content}
       </div>
     </div>
